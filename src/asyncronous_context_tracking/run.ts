da4d33d5caae571dee00695fb7e98de110186746
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-10-05 12:06:43 +0900

    asyncLocalStorage.run()の検証

---
+++ src/asyncronous_context_tracking/run.ts
@@ -0,0 +1,19 @@
+import { AsyncLocalStorage } from 'node:async_hooks';
+
+const asyncLocalStorage = new AsyncLocalStorage();
+
+const store = { id: 2 };
+
+try {
+  asyncLocalStorage.run(store, () => {
+    console.log('1', asyncLocalStorage.getStore()); // Returns the store object
+    setTimeout(() => {
+      console.log('2', asyncLocalStorage.getStore()); // Returns the store object
+    }, 200);
+
+    // エラースローした場合は、runメソッドも同じエラーをスローするようになっている
+    throw new Error();
+  });
+} catch (e) {
+  console.log('3', asyncLocalStorage.getStore()); // Returns undefined
+}
