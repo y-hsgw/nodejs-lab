fc0f14dfa7d03667c308bf95414c7bc61d1944f8
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-10-14 14:59:51 +0900

    --import flag使用しない方法のコメント追記

--- src/node_module_api/register/register-hooks.ts
+++ src/node_module_api/register/register-hooks.ts
@@ -5,3 +5,6 @@ register('./hooks.js', import.meta.url);
 // ビルド後、下記を実行すると検証可能。
 // --importフラグを使うことでapp.jsの前にregister-hooks.jsを実行することができる。
 // node --import ./dist/node_module_api/register/register-hooks.js ./dist/node_module_api/register/app.js
+
+// 下記コメントアウトを外すとnode ./dist/node_module_api/register/register-hooks.js だけでも検証可能。
+// await import('./app.js');
