f43679b7e22cf23bc520b2759c31b6a01897c39a
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-10-16 21:02:44 +0900

    findSourceMapの検証

---
+++ src/node_module_api/source_map/find_source_map.ts
@@ -0,0 +1,11 @@
+import { findSourceMap } from 'node:module';
+import path from 'node:path';
+
+const filePath = path.resolve(import.meta.dirname, './find_source_map.js');
+console.log(filePath);
+
+const sourceMap = findSourceMap(filePath);
+console.log(sourceMap.payload);
+
+// node --enable-source-maps  dist/node_module_api/source_map/find_source_map.js
+// 上記実行でSourceMapの取得可能
