2421c47def80201ad48f25cbe2fc3c570a3b2867
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-10-25 18:51:27 +0900

    console.groupの検証

---
+++ src/console/group.ts
@@ -0,0 +1,12 @@
+import console from 'node:console';
+
+console.log('This is the outer level');
+console.group();
+console.log('Level 2');
+console.group();
+console.log('Level 3');
+console.warn('More of level 3');
+console.groupEnd();
+console.log('Back to level 2');
+console.groupEnd();
+console.log('Back to the outer level');
