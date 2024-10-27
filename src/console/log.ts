3be2baacd70f21d1369f7e323a40f3a466386c4a
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-10-27 12:09:53 +0900

    console.logの検証

---
+++ src/console/log.ts
@@ -0,0 +1,9 @@
+import console from 'node:console';
+
+const count = 5;
+console.log('count: %d', count);
+
+console.log('count:', count);
+
+const string = 'string';
+console.log('Count is %d. Log is %s.', count, string);
