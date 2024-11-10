2a6753b0fe3c6b2710b3aa9716e9eca394844a52
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-11-10 12:41:11 +0900

    クリーンアップ処理記載

--- src/errors/system_errors/enotdir.ts
+++ src/errors/system_errors/enotdir.ts
@@ -4,10 +4,12 @@ import path from 'path';
 const filePath = path.join(import.meta.dirname, 'example.txt');
 
 // example.txtというファイルを作成
-fs.writeFile(filePath, 'This is a sample file.');
+await fs.writeFile(filePath, 'This is a sample file.');
 
 try {
-  fs.readdir(filePath);
+  await fs.readdir(filePath);
 } catch (err) {
   console.error(err);
+} finally {
+  await fs.unlink(filePath);
 }
