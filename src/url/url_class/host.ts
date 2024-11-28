6ad309930fcc1a1ebdaa785b8ace62051b2f23a4
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-11-28 19:57:08 +0900

    hostの検証

---
+++ src/url/url_class/host.ts
@@ -0,0 +1,7 @@
+const myURL = new URL('https://example.org:81/foo');
+console.log(myURL.host);
+// Prints example.org:81
+
+myURL.host = 'example.com:82';
+console.log(myURL.href);
+// Prints https://example.com:82/foo
