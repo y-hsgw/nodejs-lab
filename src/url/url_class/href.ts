6d8226aa8076274dd2c626db4e7c61edcb12d0fc
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-11-29 18:16:19 +0900

    hrefの検証

---
+++ src/url/url_class/href.ts
@@ -0,0 +1,7 @@
+const myURL = new URL('https://example.org/foo');
+console.log(myURL.href);
+// Prints https://example.org/foo
+
+myURL.href = 'https://example.com/bar';
+console.log(myURL.href);
+// Prints https://example.com/bar
