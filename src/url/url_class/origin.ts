d34968df0c101b51cf3a023edfca47b21f76dadf
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-11-30 11:58:05 +0900

    origin

---
+++ src/url/url_class/origin.ts
@@ -0,0 +1,11 @@
+const myURL = new URL('https://example.org/foo/bar?baz');
+console.log(myURL.origin);
+// Prints https://example.org
+
+const idnURL = new URL('https://測試');
+console.log(idnURL.origin);
+// Prints https://xn--g6w251d
+
+console.log(idnURL.hostname);
+// Prints xn--g6w251d
+// Prints https://example.org
