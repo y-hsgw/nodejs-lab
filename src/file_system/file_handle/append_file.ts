67184c4b9637c03cc2aa2b293c662a1abdcaf878
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-01-30 19:13:24 +0900

    appendFileの検証

---
+++ src/file_system/file_handle/append_file.ts
@@ -0,0 +1,16 @@
+import { open, readFile, unlink, writeFile } from 'node:fs/promises';
+import path from 'node:path';
+
+const fileName = 'thefile.txt';
+const filePath = path.join(import.meta.dirname, fileName);
+
+try {
+  await writeFile(filePath, 'hogehoge');
+  const fileHandle = await open(filePath, 'a');
+  await fileHandle.appendFile('Hello');
+  const buffer = await readFile(filePath, { encoding: 'utf-8' });
+  console.log(buffer);
+  await fileHandle.close();
+} finally {
+  await unlink(filePath);
+}
