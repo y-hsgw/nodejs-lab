b33de4d887f297b60200e7a777c2826b8a17d58a
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-02-05 20:57:42 +0900

    openの検証

---
+++ src/file_system/promise/open.ts
@@ -0,0 +1,14 @@
+import { open, readFile, unlink, writeFile } from 'node:fs/promises';
+import path from 'node:path';
+
+const fileName = 'thefile.txt';
+const filePath = path.join(import.meta.dirname, fileName);
+
+try {
+  await writeFile(filePath, 'hogehoge');
+  const fileHandle = await open(filePath, 'a');
+  console.log(fileHandle);
+  await fileHandle.close();
+} finally {
+  await unlink(filePath);
+}
