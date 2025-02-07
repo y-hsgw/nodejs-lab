bb8302cfa0a37449cbe211872fb699b5eea0adfc
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-02-07 18:49:26 +0900

    opendirの検証

---
+++ src/file_system/promise/opendir.ts
@@ -0,0 +1,8 @@
+import { opendir } from 'node:fs/promises';
+
+try {
+  const dir = await opendir('./');
+  for await (const dirent of dir) console.log(dirent.name);
+} catch (err) {
+  console.error(err);
+}
