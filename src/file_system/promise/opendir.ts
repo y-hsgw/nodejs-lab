2b7db52fee7d4572c72584f2470f95cfba0339f0
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-02-08 11:21:43 +0900

    opendirの検証方法修正

--- src/file_system/promise/opendir.ts
+++ src/file_system/promise/opendir.ts
@@ -2,7 +2,7 @@ import { opendir } from 'node:fs/promises';
 
 try {
   const dir = await opendir('./');
-  for await (const dirent of dir) console.log(dirent.name);
+  for await (const dirent of dir) console.log(dirent);
 } catch (err) {
   console.error(err);
 }
