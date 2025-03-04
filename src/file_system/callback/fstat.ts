bbd83950a55d522f1d7566422f52d4d44ea21c9d
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-03-04 20:23:32 +0900

    fstatの検証

---
+++ src/file_system/callback/fstat.ts
@@ -0,0 +1,36 @@
+import { appendFile, close, fstat, open, stat, unlink } from 'node:fs';
+import path from 'node:path';
+
+const fileName = 'thefile.txt';
+const filePath = path.join(import.meta.dirname, fileName);
+
+appendFile(filePath, 'data to append', (err) => {
+  if (err) throw err;
+  console.log('The "data to append" was appended to file!');
+
+  stat(filePath, (err, stats) => {
+    console.log(stats.mode);
+  });
+
+  open(filePath, 'r', (err, fd) => {
+    if (err) throw err;
+
+    console.log(`File ${fileName} opened successfully!`);
+
+    fstat(fd, (err, stats) => {
+      if (err) throw err;
+
+      console.log(stats.mode);
+
+      close(fd, (err) => {
+        if (err) throw err;
+        console.log(`File ${fileName} closed successfully!`);
+
+        unlink(filePath, (err) => {
+          if (err) throw err;
+          console.log(`File ${fileName} deleted successfully!`);
+        });
+      });
+    });
+  });
+});
