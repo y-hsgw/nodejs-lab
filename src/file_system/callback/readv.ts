6bf3a584900f88597b3ccbb0ab4b2a3df47a8b00
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-03-21 10:55:41 +0900

    readvの検証

---
+++ src/file_system/callback/readv.ts
@@ -0,0 +1,35 @@
+import { close, constants, open, readv, unlink, writeFile } from 'node:fs';
+import path from 'node:path';
+
+const fileName = 'thefile.txt';
+const filePath = path.join(import.meta.dirname, fileName);
+
+writeFile(filePath, '1234567890abcdefghijあいうえお', (err) => {
+  if (err) throw err;
+
+  open(filePath, 'r', constants.O_WRONLY, (err, fd) => {
+    if (err) throw err;
+
+    const buffers = [Buffer.alloc(10), Buffer.alloc(10)];
+
+    readv(fd, buffers, (err, bytesRead, buffers) => {
+      if (err) throw err;
+
+      console.log('bytesRead', bytesRead);
+      buffers.forEach((buffer) => {
+        console.log('buffer', buffer);
+        console.log('buffer', `${buffer}`);
+      });
+
+      close(fd, (err) => {
+        if (err) throw err;
+        console.log(`File ${fileName} closed successfully!`);
+
+        unlink(filePath, (err) => {
+          if (err) throw err;
+          console.log(`File ${fileName} deleted successfully!`);
+        });
+      });
+    });
+  });
+});
