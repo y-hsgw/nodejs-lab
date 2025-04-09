e4d15608479ac5868f780640cb79125c161d9a67
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-04-09 18:01:39 +0900

    asyncIteratorの挙動確認

---
+++ src/file_system/dir/async_iterator.ts
@@ -0,0 +1,10 @@
+import { opendir } from 'node:fs/promises';
+
+try {
+  const dir = await opendir('./');
+  for await (const dirent of dir) {
+    console.log(dirent);
+  }
+} catch (err) {
+  console.error(err);
+}
