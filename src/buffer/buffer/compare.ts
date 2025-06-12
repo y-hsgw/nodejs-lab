435b7bd2f077f8660f8673996b125bee6daf996e
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-06-12 20:45:10 +0900

    compareの検証

---
+++ src/buffer/buffer/compare.ts
@@ -0,0 +1,9 @@
+import { Buffer } from 'node:buffer';
+
+const buf1 = Buffer.from('1234');
+const buf2 = Buffer.from('0123');
+const arr = [buf1, buf2];
+
+console.log(arr.sort(Buffer.compare));
+// Prints: [ <Buffer 30 31 32 33>, <Buffer 31 32 33 34> ]
+// (This result is equal to: [buf2, buf1].)
