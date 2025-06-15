78cf14963348f8390bb46217a8205f276897d734
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-06-15 11:31:10 +0900

    fromの検証

---
+++ src/buffer/buffer/from.ts
@@ -0,0 +1,5 @@
+import { Buffer } from 'node:buffer';
+
+// Creates a new Buffer containing the UTF-8 bytes of the string 'buffer'.
+const buf = Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]);
+console.log(buf);
