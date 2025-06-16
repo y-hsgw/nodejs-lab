ab404b59861eb0edb4e5228357e037c77dcfe1cd
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-06-16 23:12:30 +0900

    fromの引数がArrayBufferのケース

--- src/buffer/buffer/from.ts
+++ src/buffer/buffer/from.ts
@@ -1,5 +1,28 @@
 import { Buffer } from 'node:buffer';
 
-// Creates a new Buffer containing the UTF-8 bytes of the string 'buffer'.
-const buf = Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]);
-console.log(buf);
+console.log('numberの配列');
+
+{
+  const buf = Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]);
+  console.log(buf);
+}
+
+console.log('ArrayBuffer');
+
+{
+  const arr = new Uint16Array(2);
+
+  arr[0] = 5000;
+  arr[1] = 4000;
+
+  const buf1 = Buffer.from(arr.buffer);
+
+  console.log(buf1);
+
+  arr[1] = 6000;
+
+  console.log(buf1);
+
+  const buf2 = Buffer.from(arr.buffer, 1, 2);
+  console.log(buf2);
+}
