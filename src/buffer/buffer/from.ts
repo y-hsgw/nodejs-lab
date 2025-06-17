c69c803283480138fd003ab03923261f2b548baf
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-06-17 22:24:50 +0900

    引数がbufferのケース検証

--- src/buffer/buffer/from.ts
+++ src/buffer/buffer/from.ts
@@ -1,13 +1,13 @@
 import { Buffer } from 'node:buffer';
 
-console.log('numberの配列');
+console.log('-----numberの配列-----');
 
 {
   const buf = Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]);
   console.log(buf);
 }
 
-console.log('ArrayBuffer');
+console.log('-----ArrayBuffer-----');
 
 {
   const arr = new Uint16Array(2);
@@ -25,4 +25,15 @@ console.log('ArrayBuffer');
 
   const buf2 = Buffer.from(arr.buffer, 1, 2);
   console.log(buf2);
+}
+
+console.log('-----buffer-----');
+{
+  const buf1 = Buffer.from('buffer');
+  const buf2 = Buffer.from(buf1);
+
+  buf1[0] = 0x61;
+
+  console.log(buf1.toString());
+  console.log(buf2.toString());
 }
