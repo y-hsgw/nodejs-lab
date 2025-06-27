c8830bb8ef7ef4181b4fc3abc208bc29719b083c
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-06-27 18:55:34 +0900

    entriesの検証

---
+++ src/buffer/buffer/entries.ts
@@ -0,0 +1,16 @@
+import { Buffer } from 'node:buffer';
+
+// Log the entire contents of a `Buffer`.
+
+const buf = Buffer.from('buffer');
+
+for (const pair of buf.entries()) {
+  console.log(pair);
+}
+// Prints:
+//   [0, 98]
+//   [1, 117]
+//   [2, 102]
+//   [3, 102]
+//   [4, 101]
+//   [5, 114]
