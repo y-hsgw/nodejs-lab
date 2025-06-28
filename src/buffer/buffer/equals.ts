5131af01cc767a30443170805a92fc618127230f
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-06-28 11:50:06 +0900

    equalsの検証

---
+++ src/buffer/buffer/equals.ts
@@ -0,0 +1,10 @@
+import { Buffer } from 'node:buffer';
+
+const buf1 = Buffer.from('ABC');
+const buf2 = Buffer.from('414243', 'hex');
+const buf3 = Buffer.from('ABCD');
+
+console.log(buf1.equals(buf2));
+// Prints: true
+console.log(buf1.equals(buf3));
+// Prints: false
