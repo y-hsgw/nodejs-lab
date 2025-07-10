888f9bae9ddd66a2081b39e89d1c33e1a263b5cc
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-07-10 21:06:53 +0900

    readDoubleLEの検証

---
+++ src/buffer/buffer/read_double_LE.ts
@@ -0,0 +1,8 @@
+import { Buffer } from 'node:buffer';
+
+const buf = Buffer.from([1, 2, 3, 4, 5, 6, 7, 8]);
+
+console.log(buf.readDoubleLE(0));
+// Prints: 5.447603722011605e-270
+console.log(buf.readDoubleLE(1));
+// Throws ERR_OUT_OF_RANGE.
