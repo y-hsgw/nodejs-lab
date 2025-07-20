7bb28e365f24775747784e851a3e831893840bdf
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-07-20 10:46:24 +0900

    readUInt8の検証

---
+++ src/buffer/buffer/read_u_int8.ts
@@ -0,0 +1,10 @@
+import { Buffer } from 'node:buffer';
+
+const buf = Buffer.from([1, -2]);
+
+console.log(buf.readUInt8(0));
+// Prints: 1
+console.log(buf.readUInt8(1));
+// Prints: 254
+console.log(buf.readUInt8(2));
+// Throws ERR_OUT_OF_RANGE.
