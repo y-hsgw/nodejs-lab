23787f0052d6e76f90145828212f481d4aeb312f
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-07-29 23:32:52 +0900

    swap32の検証

---
+++ src/buffer/buffer/swap32.ts
@@ -0,0 +1,16 @@
+import { Buffer } from 'node:buffer';
+
+const buf1 = Buffer.from([0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8]);
+
+console.log(buf1);
+// Prints: <Buffer 01 02 03 04 05 06 07 08>
+
+buf1.swap32();
+
+console.log(buf1);
+// Prints: <Buffer 04 03 02 01 08 07 06 05>
+
+const buf2 = Buffer.from([0x1, 0x2, 0x3]);
+
+buf2.swap32();
+// Throws ERR_INVALID_BUFFER_SIZE.
