2428bf3594ce1d29051b28d2bc59c70ff5eedcd7
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-08-02 10:21:08 +0900

    valuesの検証

---
+++ src/buffer/buffer/values.ts
@@ -0,0 +1,25 @@
+import { Buffer } from 'node:buffer';
+
+const buf = Buffer.from('buffer');
+
+for (const value of buf.values()) {
+  console.log(value);
+}
+// Prints:
+//   98
+//   117
+//   102
+//   102
+//   101
+//   114
+
+for (const value of buf) {
+  console.log(value);
+}
+// Prints:
+//   98
+//   117
+//   102
+//   102
+//   101
+//   114
