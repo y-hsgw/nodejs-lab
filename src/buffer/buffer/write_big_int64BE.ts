2958aaabf68d87a901ce1b65570771ef6b948aff
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-08-04 21:58:09 +0900

    writeBigInt64BEの検証

---
+++ src/buffer/buffer/write_big_int64BE.ts
@@ -0,0 +1,8 @@
+import { Buffer } from 'node:buffer';
+
+const buf = Buffer.allocUnsafe(8);
+
+buf.writeBigInt64BE(0x0102030405060708n, 0);
+
+console.log(buf);
+// Prints: <Buffer 01 02 03 04 05 06 07 08>
