b5fbaab2a74eb7b142196afe9afccdc9ebc263f3
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-08-16 09:49:41 +0900

    writeUInt8の検証

---
+++ src/buffer/buffer/write_u_int8.ts
@@ -0,0 +1,11 @@
+import { Buffer } from 'node:buffer';
+
+const buf = Buffer.allocUnsafe(4);
+
+buf.writeUInt8(0x3, 0);
+buf.writeUInt8(0x4, 1);
+buf.writeUInt8(0x23, 2);
+buf.writeUInt8(0x42, 3);
+
+console.log(buf);
+// Prints: <Buffer 03 04 23 42>
