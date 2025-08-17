77daf477b07c00630edd275a4ca236906a408a9a
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-08-17 13:59:59 +0900

    writeUInt16BEの検証

---
+++ src/buffer/buffer/write_u_int16BE.ts
@@ -0,0 +1,9 @@
+import { Buffer } from 'node:buffer';
+
+const buf = Buffer.allocUnsafe(4);
+
+buf.writeUInt16BE(0xdead, 0);
+buf.writeUInt16BE(0xbeef, 2);
+
+console.log(buf);
+// Prints: <Buffer de ad be ef>
