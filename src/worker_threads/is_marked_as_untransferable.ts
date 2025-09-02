056555ec773a7c36256aadc0dff02495c50e9c8e
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-09-02 11:25:04 +0900

    isMarkedAsUntransferableの検証

---
+++ src/worker_threads/is_marked_as_untransferable.ts
@@ -0,0 +1,7 @@
+import { markAsUntransferable, isMarkedAsUntransferable } from 'node:worker_threads';
+
+const pooledBuffer = new ArrayBuffer(8);
+markAsUntransferable(pooledBuffer);
+
+console.log(isMarkedAsUntransferable(pooledBuffer));
+console.log(isMarkedAsUntransferable({}));
