7b4ecd5f5f194a878e8256391fa11338d5d0272d
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-09-06 16:56:56 +0900

    postMessageToThreadの検証

---
+++ src/worker_threads/post_message_to_thread.ts
@@ -0,0 +1,34 @@
+import process from 'node:process';
+import {
+  // @ts-expect-error
+  postMessageToThread,
+  threadId,
+  workerData,
+  Worker,
+} from 'node:worker_threads';
+
+const channel = new BroadcastChannel('sync');
+const level = workerData?.level ?? 0;
+console.log(workerData);
+if (level < 10) {
+  new Worker(new URL(import.meta.url), {
+    workerData: { level: level + 1 },
+  });
+}
+
+if (level === 0) {
+  process.on('workerMessage', (value, source) => {
+    console.log(`${source} -> ${threadId}:`, value);
+    postMessageToThread(source, { message: 'pong' });
+  });
+} else if (level === 10) {
+  process.on('workerMessage', (value, source) => {
+    console.log(`${source} -> ${threadId}:`, value);
+    channel.postMessage('done');
+    channel.close();
+  });
+
+  await postMessageToThread(0, { message: 'ping' });
+}
+
+channel.onmessage = channel.close;
