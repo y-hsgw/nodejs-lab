1893383b4a271d8aa07ad27a3d5745bddd3c0fec
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-09-11 21:55:19 +0900

    BroadcastChannelの検証

---
+++ src/worker_threads/broadcast_channel/broadcast_channel_class.ts
@@ -0,0 +1,19 @@
+import { isMainThread, BroadcastChannel, Worker } from 'node:worker_threads';
+
+const bc = new BroadcastChannel('hello');
+
+console.log('isMainThread', isMainThread);
+
+if (isMainThread) {
+  let c = 0;
+  bc.onmessage = (event) => {
+    console.log(c);
+    // @ts-expect-error
+    console.log(event.data);
+    if (++c === 10) bc.close();
+  };
+  for (let n = 0; n < 10; n++) new Worker(import.meta.filename);
+} else {
+  bc.postMessage('hello from every worker');
+  bc.close();
+}
