fd83fb69a38cbb25cd9f9f6c83540c2b1b3f6475
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-09-14 15:05:06 +0900

    MessageChannelクラスの検証

---
+++ src/worker_threads/message_channel/message_channel_class.ts
@@ -0,0 +1,6 @@
+import { MessageChannel } from 'node:worker_threads';
+
+const { port1, port2 } = new MessageChannel();
+port1.on('message', (message) => console.log('received', message));
+port2.postMessage({ foo: 'bar' });
+port2.close();
