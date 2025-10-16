7365aaa02d263d1d60f7c20531195de488031feb
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-10-16 19:52:43 +0900

    beforeExitイベントの検証

---
+++ src/process/event/before_exit.ts
@@ -0,0 +1,16 @@
+import process from 'node:process';
+
+process.on('beforeExit', (code) => {
+  console.log('Process beforeExit event with code: ', code);
+});
+
+process.on('exit', (code) => {
+  console.log('Process exit event with code: ', code);
+});
+
+console.log('This message is displayed first.');
+
+// Prints:
+// This message is displayed first.
+// Process beforeExit event with code: 0
+// Process exit event with code: 0
