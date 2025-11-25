c7f856fa60e66d7d02f7d955605a35a5ae7df8a4
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-11-25 21:18:53 +0900

    registerBeforeExitの検証

---
+++ src/process/finalization/register_before_exit.ts
@@ -0,0 +1,12 @@
+import { finalization } from 'node:process';
+
+function onFinalize(obj: object, event: string) {
+  console.log(obj);
+  console.log(event);
+}
+
+const resource = {
+  hoge: 'foo',
+};
+
+finalization.registerBeforeExit(resource, onFinalize);
