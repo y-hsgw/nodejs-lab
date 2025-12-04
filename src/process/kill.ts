476a537d59d9f19ff057bcf7c18b0c6d5b576cc8
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-12-04 20:54:21 +0900

    killの検証

---
+++ src/process/kill.ts
@@ -0,0 +1,12 @@
+import process, { kill } from 'node:process';
+
+process.on('SIGHUP', () => {
+  console.log('Got SIGHUP signal.');
+});
+
+setTimeout(() => {
+  console.log('Exiting.');
+  process.exit(0);
+}, 100);
+
+kill(process.pid, 'SIGHUP');
