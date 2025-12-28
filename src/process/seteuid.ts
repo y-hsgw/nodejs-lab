30e29b966d7805e87fb62342b464afd6115b5627
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2025-12-28 09:37:59 +0900

    seteuidの検証

---
+++ src/process/seteuid.ts
@@ -0,0 +1,11 @@
+import process from 'node:process';
+
+if (process.geteuid && process.seteuid) {
+  console.log(`Current uid: ${process.geteuid()}`);
+  try {
+    process.seteuid(501);
+    console.log(`New uid: ${process.geteuid()}`);
+  } catch (err) {
+    console.error(`Failed to set uid: ${err}`);
+  }
+}
