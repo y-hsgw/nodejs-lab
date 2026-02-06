c72bd2fd5ddc7ffb89e046cc2101fe3fb3961ba8
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2026-02-06 21:04:18 +0900

    respondの検証

---
+++ src/web_streams/readable_stream_byob_request/respond.ts
@@ -0,0 +1,53 @@
+import { ReadableStream } from 'node:stream/web';
+
+{
+  const stream = new ReadableStream({
+    type: 'bytes',
+    pull(controller) {
+      const req = controller.byobRequest;
+      console.log(req);
+      if (!req) return;
+
+      const v = req.view!;
+      const u8 = new Uint8Array(v.buffer, v.byteOffset, v.byteLength);
+      u8[0] = 0x41; // 'A'
+      u8[1] = 0x42; // 'B'
+      req.respond(2);
+      controller.close();
+    },
+  });
+
+  const reader = stream.getReader({ mode: 'byob' });
+
+  const buf = new Uint8Array(10);
+
+  const { value, done } = await reader.read(buf);
+  console.log(done); // false
+  console.log(Array.from(value!)); // [65, 66]
+}
+
+{
+  const stream = new ReadableStream({
+    type: 'bytes',
+    pull(controller) {
+      const req = controller.byobRequest;
+      console.log(req);
+      if (!req) return;
+
+      const v = req.view!;
+      const u8 = new Uint8Array(v.buffer, v.byteOffset, v.byteLength);
+      u8[0] = 0x41; // 'A'
+      u8[1] = 0x42; // 'B'
+      req.respond(1);
+      controller.close();
+    },
+  });
+
+  const reader = stream.getReader({ mode: 'byob' });
+
+  const buf = new Uint8Array(10);
+
+  const { value, done } = await reader.read(buf);
+  console.log(done); // false
+  console.log(Array.from(value!)); // [65]
+}
