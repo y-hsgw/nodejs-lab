d6e94779ee8be4647a61af4b1641f2272d3be23f
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2026-03-17 20:24:08 +0900

    readableの検証

---
+++ src/web_streams/text_encoder_stream/readable.ts
@@ -0,0 +1,14 @@
+import { TextEncoderStream } from 'node:stream/web';
+
+const textEncoderStream = new TextEncoderStream();
+
+console.log(textEncoderStream.readable);
+
+const writer = textEncoderStream.writable.getWriter();
+const reader = textEncoderStream.readable.getReader();
+
+writer.write('Hello');
+writer.close();
+
+const { value } = await reader.read();
+console.log(value); // Uint8Array(5) [ 72, 101, 108, 108, 111 ]
